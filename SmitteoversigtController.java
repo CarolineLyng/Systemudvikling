71933db10343e1fff081bae2aa52624434c46b94
CarolineLyng <80045593+CarolineLyng@users.noreply.github.com>
2021-06-22 09:01:48 +0200

    Create SmitteoversigtController.java

---
+++ SmitteoversigtController.java
@@ -0,0 +1,22 @@
+package sample;
+
+
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+public class SmitteoversigtController {
+
+    public Label labelAntalSmittede;
+    public Label labelInfektionsRate;
+    public Label labelMutationstyper;
+    public Label labelZipCode;
+    public Label labelDato;
+}
